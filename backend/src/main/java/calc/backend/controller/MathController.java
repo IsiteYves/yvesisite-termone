feead2c68c67cc9160402cfac182b8f4ef3da4b5
IsiteYves <yvesisite@gmail.com>
2022-12-13 13:46:47 +0200

    feat: FE refactor

--- backend/src/main/java/calc/backend/controller/MathController.java
+++ backend/src/main/java/calc/backend/controller/MathController.java
@@ -4,14 +4,15 @@ import calc.backend.utils.CalcResponse;
 import calc.backend.dto.DoMathRequest;
 import calc.backend.exceptions.InvalidOperationException;
 import calc.backend.impl.MathOperatorImpl;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping( "/")
 public class MathController {
     MathOperatorImpl mathOperatorImpl = new MathOperatorImpl();
 
-    @PostMapping
+    @RequestMapping(value="/", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, method = RequestMethod.POST)
     @ResponseBody
     public CalcResponse doMath(@RequestBody DoMathRequest doMathRequest) throws InvalidOperationException {
         double result = mathOperatorImpl.doMath(doMathRequest.getOperand1(), doMathRequest.getOperand2(), doMathRequest.getOperation());
