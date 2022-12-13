2f48b843f0c915c133bdc2d9da3944b061a0fcf9
IsiteYves <yvesisite@gmail.com>
2022-12-13 13:49:35 +0200

    FIX: initial resolve of HttpMediaTypeNotSupportedException attempt

--- backend/src/main/java/calc/backend/controller/MathController.java
+++ backend/src/main/java/calc/backend/controller/MathController.java
@@ -4,15 +4,14 @@ import calc.backend.utils.CalcResponse;
 import calc.backend.dto.DoMathRequest;
 import calc.backend.exceptions.InvalidOperationException;
 import calc.backend.impl.MathOperatorImpl;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping( "/")
+@RequestMapping("/")
 public class MathController {
     MathOperatorImpl mathOperatorImpl = new MathOperatorImpl();
 
-    @RequestMapping(value="/", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, method = RequestMethod.POST)
+    @PostMapping
     @ResponseBody
     public CalcResponse doMath(@RequestBody DoMathRequest doMathRequest) throws InvalidOperationException {
         double result = mathOperatorImpl.doMath(doMathRequest.getOperand1(), doMathRequest.getOperand2(), doMathRequest.getOperation());
